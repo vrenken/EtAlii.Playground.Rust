87d80aa08645a0eec5dc0ca8e8f301d96e5d74e8
vrenken <github@vrenken.eu>
2025-11-03 20:26:20 +0100

    Expand configuration to include logging settings
    - Added `Logging` struct to manage log-related configuration, including OTLP endpoint and Seq API key.
    - Updated `Configuration` struct to publicly expose `application` and new `logging` fields.
    - Implemented `setup` function to load and deserialize configuration using `config` crate.
    - Included support for environment variables prefixed with `APP` in configuration loading.

--- src/configuration/configuration.rs
+++ src/configuration/configuration.rs
@@ -1,12 +1,34 @@
-﻿use serde::Deserialize;
+﻿use config::{Config, Environment, File};
+use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Configuration {
-    application: Application,
+    pub application: Application,
+    pub logging: Logging
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Application {
-    name: String,
-    log_level: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Logging {
+    pub log_level: String,
+    pub otlp_endpoint: String,
+    pub seq_api_key: String,
+}
+
+pub fn setup() -> Configuration {
+    let config = Config::builder()
+        .add_source(File::with_name("configuration"))
+        // .add_source(File::with_name("configuration.production").required(false))
+        .add_source(File::with_name("configuration.development").required(false))
+        .add_source(Environment::with_prefix("APP").separator("_"))
+        .build()
+        .unwrap();
+
+    tracing::info!("Loaded configuration");
+
+    config.try_deserialize().unwrap()
 }
