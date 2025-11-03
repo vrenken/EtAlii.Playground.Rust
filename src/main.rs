c3731758c9e06eab5b408b5914321e8cca6eddef
vrenken <github@vrenken.eu>
2025-11-03 20:34:00 +0100

    Enhance logging and error handling in `main.rs`
    - Replaced `println!` with `tracing` for cleaner and structured logging.
    - Integrated `tracing-subscriber` for advanced logging capabilities.
    - Added `anyhow` for improved error handling.
    - Refactored application initialization to include tracing setup.
    - Streamlined configuration loading with a dedicated `setup` function.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,42 @@
+use config::{File, Environment};
+use axum::{ routing::{get, post}, Router };
+use std::sync::{Arc, Mutex};
+use tracing::*;
+use tracing_subscriber::EnvFilter;
+
 mod data;
 use data::*;
 
 mod service;
 
 mod configuration;
-use configuration::*;
-
 mod portal;
+
 use portal::dashboard::*;
 use portal::items::*;
 use portal::input::*;
 
-use config::{Config, File, Environment};
-
-use axum::{
-    routing::{get, post},
-    Router,
-};
-use std::sync::{Arc, Mutex};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
 
-    let config = Config::builder()
-        .add_source(File::with_name("configuration"))
-        // .add_source(File::with_name("configuration.production").required(false))
-        .add_source(File::with_name("configuration.development").required(false))
-        .add_source(Environment::with_prefix("APP").separator("_"))
-        .build()
-        .unwrap();
+    // Initialize tracing for console output
+    tracing_subscriber::fmt()
+        .compact()
+        //.pretty()
+        .with_env_filter(EnvFilter::from_default_env()
+            .add_directive("trace".parse()?)) // fallback filter
+        .with_target(true)
+        .with_thread_ids(false)
+        .with_thread_names(false)
+        .init();
 
-    let configuration: Configuration = config.try_deserialize().unwrap();
+    info!("Starting application...");
+
+    // === Configuration.
+    let configuration = configuration::setup();
+
+    tracing::info!("Creating app state");
 
     let items = vec![
         ListItem::new("Milk", 2),
@@ -44,6 +50,7 @@ async fn main() {
         name: Arc::new(Mutex::new(String::from(""))),
     };
 
+    tracing::info!("Setting up tokio router");
     let app = Router::new()
         .route("/", get(portal::dashboard::get_page))
         .route("/dashboard/cpu", get(get_cpu))
@@ -59,7 +66,9 @@ async fn main() {
 
     // run our app with hyper, listening globally on port 3000
     let endpoint = "0.0.0.0:3000";
-    let listener = tokio::net::TcpListener::bind(endpoint).await.unwrap();
-    println!("listening on http://{}", endpoint.replace("0.0.0.0", "127.0.0.1"));
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(endpoint).await?;
+    tracing::info!("listening on http://{}", endpoint.replace("0.0.0.0", "127.0.0.1"));
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
